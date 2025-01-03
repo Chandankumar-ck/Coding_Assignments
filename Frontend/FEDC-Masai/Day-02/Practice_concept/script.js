7e29a9a33da5c6a2f62bd0c7f32bbb11ecf92943
CHANDAN KUMAR MALLICK <chandankumarchandan.1111@gmail.com>
2025-01-03 17:48:30 +0530

    task complete

---
+++ Frontend/FEDC-Masai/Day-02/Practice_concept/script.js
@@ -0,0 +1,22 @@
+// for loop
+// while loop
+// for...in loop (for Objects)
+// for...of loop (for Arrays and Strings)
+
+// For Loops default writting style 
+
+// for (let i =0; i<=10;i++){
+//     console.log(i)
+// }
+
+// for of uses 
+// --------------------------------------------------
+// let bag = "";
+// let arr = ['chandan', 'kumar', 'malik', 10, 20, 30];
+
+// for (let i of arr){
+//     bag+=i +" ";
+    
+// }
+// console.log(bag);
+
